10c3ea9cc7ee1c0fec52e8519677e9ad391f7354
hkagei <k_dawg_1022@hotmail.com>
2022-06-28 20:52:13 -0600

    rewrite code structure

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const connection = require('./connection')
+
+class Data {
+    constructor(connection){
+        this.connection=connection;
+    }
+
+getAllEmployees() {
+    return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;")
+}
+
+getAllDepartments() {
+    return this.connection.promise().query("SELECT department.id, department.name FROM department;")
+}
+
+getAllRoles() {
+    return this.connection.promise().query("SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id=deparment.id;")
+}
+
+}
--- server.js
+++ server.js
@@ -1,24 +1,171 @@
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const consoleTable = require('console.table');
+const { prompt } = require('inquirer');
+require('console.table');
+const db = require('./db');
 
 initialPrompt();
 
 function initialPrompt() {
-    inquirer.prompt(
-    {
-    type: 'input',
+    prompt([{
+    type: 'list',
     name: 'username',
     message: "What would you like to do?",
-    choices: ["MIT", "Apache", "Boost", "None"]
-    }
-    )
+    choices: [
+        {
+            name: "View All Departments",
+            value: "VIEW_ALL_DEPARTMENTS"},
+        { 
+            name: "View All Roles", 
+            value: "VIEW_ALL_ROLES"}, 
+        {
+            name: "View All Employees", 
+            value: "VIEW_ALL_EMPLOYEES"},
+        {
+            name: "Add a Department",
+            value: "ADD_A_DEPARTMENT"},
+        {
+            name: "Add a Role",
+            value: "ADD_A_ROLE"},
+        {
+            name: "Add an Employee",
+            value: "ADD_AN_EMPLOYEE"},
+        {
+            name: "Update an Employee Role",
+            value: "UPDATE_AN_EMPLOYEE_ROLE"},
+        {
+            name: "Exit",
+            value: "EXIT"
+        }
+    ]
+    }]).then(res => {
+        let option = res.option;
+        switch(option) {
+            case "VIEW_ALL_DEPARTMENTS":
+            viewDepartments();
+            break;
+            case "VIEW_ALL_ROLES":
+            viewRoles();
+            break;
+            case "VIEW_ALL_EMPLOYEES":
+            viewEmployees();
+            break;
+            case "ADD_A_DEPARTMENT":
+            addDepartment();
+            break;
+            case "ADD_A_ROLE":
+            addRole();
+            break;
+            case "ADD_AN_EMPLOYEE":
+            addEmployee();
+            break;
+            case "UPDATE_AN_EMPLOYEE_ROLE":
+            updateEmployee();
+            break
+            
+            default:
+                quit();
+
+        }
+    })
 }
 
-function viewDepartment() {
-    db.query('SELECT * FROM department', function (error, data) {
-        if (error) throw error;
-        console.table(data)
-    })
-    initialPrompt();
+function viewDepartments() {
+    db.getAllDepartments().then(([rows]) => {
+        let departments = rows;
+        console.log('\n');
+        console.table(departments)
+    }).then(() => initialPrompt())
+};
+
+function viewRoles() {
+    db.getAllRoles().then(([rows]) => {
+        let roles = rows;
+        console.log('\n');
+        console.table(rows)
+    }).then(() => initialPrompt())
+};
+
+// a function to view the employees 
+// it goes to index.js within db and then finds a function called getAllEmployees that has the sql commands needed to actually 
+function viewEmployees() {
+    db.getAllEmployees().then(([rows]) => {
+        let employees = rows;
+        console.log('\n');
+        console.table(employees)
+    }).then(() => initialPrompt())
+};
+
+function addDepartment() {
+    prompt([
+        {
+          name: "name",
+          message: "What is the name of the department?"
+        }
+      ])
+        .then(res => {
+          let name = res;
+          db.createDepartment(name)
+            .then(() => console.log(`Added ${name.name} to the database`))
+            .then(() => loadMainPrompts())
+        })
+};
+
+function addRole() {
+    prompt([
+        {
+          name: "name",
+          message: "What is the employee's role?"
+        }
+      ])
+        .then(res => {
+          let name = res;
+          db.createRole(name)
+            .then(() => console.log(`Added ${name.name} to the database`))
+            .then(() => loadMainPrompts())
+        })
+    }
+
+function addEmployee() {
+    prompt([
+        {
+          name: "name",
+          message: "What is the employe's name?"
+        }
+      ])
+        .then(res => {
+          let name = res;
+          db.createDepartment(name)
+            .then(() => console.log(`Added ${name.name} to the database`))
+            .then(() => loadMainPrompts())
+        })
 }
+
+function quit() {
+    process.exit();
+}
+
+// function initialPrompt() {
+//     inquirer.prompt(
+//     {
+//     type: 'list',
+//     name: 'username',
+//     message: "What would you like to do?",
+//     choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"]
+//     }
+//     )
+// }
+
+// function viewDepartments() {
+//     db.query('SELECT * FROM department', function (error, data) {
+//         if (error) throw error;
+//         console.table(data)
+//     })
+//     initialPrompt();
+// }
+
+// function viewRoles() {
+//     db.query('SELECT role.*, department.dep_name AS dep')
+// }
+
+// function viewEmployees() {
+//     db.query
+// }
